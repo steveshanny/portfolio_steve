1b8494394a91459a4465c4b8ca7167880ce013d3
steverasoafanirindraibe <steveshannyrasoafanirindraibe@gmail.com>
2026-07-30 11:50:33 +0300

    fix(SEO): Add base URL in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export const dynamic = 'force-static';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://localhost:3000';
+  const baseUrl = 'https://steveshanny.vercel.app';
 
   return [
     {
